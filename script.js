745e5c1dffd82c4bf37a75bc1aa41df453067f0e
kristibyrnes <kristibyrnes@icloud.com>
2018-01-30 20:37:00 -0500

    lab3

--- script.js
+++ script.js
@@ -1,55 +1,82 @@
-var userHealth = 40;
-var grantHealth = 10;
-var userWins = 0;
-var grantWins = 0;
-var playing = true;
-var userName;
+(function() {
+
+var player = {
+  wins: 0,
+  health: 40,
+  healsRemaining: 2,
+  name: "",
+
+  generateAttackDamage: function() {
+    return Math.floor(Math.random() * 3) + 1;
+  },
+  heal: function(){
+    var heal = Math.floor((Math.random() * 10) + 1);
+    return heal;
+  }
+}
+
+var grant = {
+  name: "Grant the Almighty Chirpus",
+  health: 10,
+
+  generateAttackDamage: function(){
+    return Math.floor(Math.random() * 5) + 1;
+  }
+}
 
 startGame();
 
 function startGame() {
   var play = prompt("Do you want to play?");
   if (play !== "no"){
-    userName = prompt("Enter your character name:");
+    player.name = prompt("Enter your character name:");
     startCombat();
   } else {
     console.log("You must fight! Bye!");
-    playing = false;
   }
 }
 
-function getDamage(){
-  var health = Math.floor(Math.random() * 5) + 1;
-  return health;
-}
-
-function startCombat(){
-  while (playing) {
-    if (userWins === 3) {
-     console.log(userName + " Wins!");
+function startCombat() {
+  while (player.wins < 6) {
+   if (player.wins === 5) {
+     console.log("YASSSSS, " + player.name + " wins!");
      break;
-    } else if (grantWins === 1) {
-     console.log("Grant the Mighty Chicken has won!");
-     break;
-    }
-
-     var optionToFight = prompt("Do you want to attack or quit?");
-     if (optionToFight.toLowerCase() === "attack") {
-       userHealth -= getDamage();
-       grantHealth -= getDamage();
-       console.log(userName + " has " + userHealth + " left.");
-       console.log("Grant the Mighty Chicken has " + grantHealth + " left.");
-     } else {
-       console.log("You quit! Grant the Mighty Chicken has won!");
-       break;
-     } if (grantHealth <= 0) {
-     userWins++;
-     grantHealth = 10;
-     console.log("You have beat Grant! You need to win " + (3 - userWins) + " more round(s)." );
-
-   } else if (userHealth <= 0) {
-     console.log("You lose.");
    }
-  }
 
+     //attack
+     var optionToFight = prompt("Do you want to attack, heal or quit?");
+     if (optionToFight.toLowerCase() === "attack") {
+      player.health -= player.generateAttackDamage();
+      grant.health -= grant.generateAttackDamage();
+        console.log(player.name + " has " + player.health + " left.");
+        console.log("Grant the Mighty Chicken has " + grant.health + " left.");
+      }
+
+      //heal
+     if (optionToFight.toLowerCase() === "heal") {
+       if (player.healsRemaining > 0){
+        player.health += player.heal();
+        player.healsRemaining--;
+          console.log(player.name + " has healed and has " + player.health + " left!");
+        } else {
+          console.log("Uh-Oh! You have used up all of your heals ):");
+        }
+      }
+
+      //quit
+      if (optionToFight.toLowerCase() === "quit"){
+      console.log("You quit! Grant the Mighty Chicken has won!");
+      break;
+      }
+
+      if (grant.health <= 0) {
+        player.wins++;
+        grant.health = 10;
+        console.log("You have beat Grant! You need to win " + (5 - player.wins) + " more round(s)." );
+        } else if (player.health <= 0) {
+          console.log("You lose ):");
+      }
+  }
 }
+
+})();
